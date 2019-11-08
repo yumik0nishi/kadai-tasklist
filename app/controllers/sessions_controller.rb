4eb1f51e94a06bd08448b71240cfa15c60ceb173
yumi.konishi <yuko1213n@yahoo.co.jp>
2019-11-08 11:08:37 +0000

    routes,logout-link retouch

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:success] = 'ログアウトしました。'
-    render :new
+    redirect_to root_url
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users
+  resources :users, only: [:new, :create]
   
   resources :tasks
   
