a9d7f4f7290bd0e96384b58704178c4c9c57a07f
yumi.konishi <yuko1213n@yahoo.co.jp>
2019-11-08 10:20:09 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
+  get 'signup', to: 'users#new'
   resources :users
   
   resources :tasks
